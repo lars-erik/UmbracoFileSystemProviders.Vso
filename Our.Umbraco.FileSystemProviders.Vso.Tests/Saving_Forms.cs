9112c632589be1c48a132df4d1baf344b004851a
Lars-Erik Aabech <lea@markedspartner.no>
2016-09-12 09:06:15 +0200

    Add project files.

---
+++ Our.Umbraco.FileSystemProviders.Vso.Tests/Saving_Forms.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Moq;
+using NUnit.Framework;
+using Umbraco.Core.IO;
+
+namespace Umbraco.Forms.Git.Tests
+{
+    [TestFixture]
+    public class Saving_Files
+    {
+        private Mock<GitHttpClientBase> gitClientMock;
+        private VsoGitFileSystemProvider provider;
+        private string path;
+        private MemoryStream memoryStream;
+        const string expectedOldObjectId = "8AE7FC51-2175-4423-8C09-1CF454367353";
+        const string repositoryId = "C7FD20A1-9821-40F1-9F0B-10BB1360F43A";
+        const string repoRoot = "/My.WebSite";
+        private const string expectedContents = "{\"id\":\"7b9487c3-7a66-4187-a049-e0213389e0a3\"}";
+        private const string expectedPath = "/App_Plugins/UmbracoForms/Data/Forms/b79a3cc8-533c-41a9-bcd2-2e9210c7c010.json";
+
+        [SetUp]
+        public void Setup()
+        {
+            var wrapped = Mock.Of<IFileSystem>();
+            gitClientMock = new Mock<GitHttpClientBase>(new Uri("http://localhost"), new VssCredentials());
+            var gitClient = gitClientMock.Object;
+            provider = new VsoGitFileSystemProvider(wrapped, gitClient, repositoryId, repoRoot);
+            path = "~" + expectedPath;
+
+            memoryStream = new MemoryStream();
+            var writer = new StreamWriter(memoryStream);
+            writer.Write(expectedContents);
+            writer.Flush();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            gitClientMock.Setup(c => c.GetCommitsAsync(repositoryId, It.IsAny<GitQueryCommitsCriteria>(), It.IsAny<int?>(), It.IsAny<int?>(), It.IsAny<object>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<GitCommitRef>
+                {
+                    new GitCommitRef
+                    {
+                        CommitId = expectedOldObjectId
+                    }
+                });
+        }
+
+        [Test]
+        public void Commits_Changed_File_To_Git()
+        {
+            provider.AddFile(path, memoryStream);
+
+            VerifyCommit(VersionControlChangeType.Edit, "Changed from backoffice");
+        }
+
+        [Test]
+        public void Commits_New_File_To_Git()
+        {
+            gitClientMock
+                .Setup(c => c.GetItemAsync(repositoryId, repoRoot + expectedPath, It.IsAny<string>(), It.IsAny<VersionControlRecursionType?>(), It.IsAny<bool?>(), It.IsAny<bool?>(), It.IsAny<bool?>(), It.IsAny<GitVersionDescriptor>(), It.IsAny<object>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new VssServiceException());
+
+            provider.AddFile(path, memoryStream);
+
+            gitClientMock
+                .Verify(c => c.GetItemAsync(repositoryId, repoRoot + expectedPath, It.IsAny<string>(), It.IsAny<VersionControlRecursionType?>(), It.IsAny<bool?>(), It.IsAny<bool?>(), It.IsAny<bool?>(), It.IsAny<GitVersionDescriptor>(), It.IsAny<object>(), It.IsAny<CancellationToken>()));
+
+            VerifyCommit(VersionControlChangeType.Add, "Added from backoffice");
+        }
+
+        private void VerifyCommit(VersionControlChangeType changeType, string message)
+        {
+            gitClientMock.Verify(c => c.CreatePushAsync(
+                Match.Create<GitPush>(gp =>
+                    VerifyCommit(gp, changeType, message)
+                    ),
+                repositoryId,
+                It.IsAny<object>(),
+                It.IsAny<CancellationToken>()
+                ));
+        }
+
+        private bool VerifyCommit(GitPush gp, VersionControlChangeType changeType, string message)
+        {
+            //gp.RefUpdates.Single().RepositoryId == new Guid(repositoryId) &&
+            var result = gp.RefUpdates.Single().OldObjectId == expectedOldObjectId &&
+                               gp.Commits.Single().Comment == message &&
+                               gp.Commits.Single().Changes.Single().ChangeType == changeType &&
+                               gp.Commits.Single().Changes.Single().Item.Path == repoRoot + expectedPath &&
+                               gp.Commits.Single().Changes.Single().NewContent.ContentType == ItemContentType.RawText &&
+                               gp.Commits.Single().Changes.Single().NewContent.Content == expectedContents;
+            return result;
+        }
+    }
+}
---
+++ Our.Umbraco.FileSystemProviders.Vso/VsoGitFileSystemProvider.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Umbraco.Core.IO;
+
+namespace Umbraco.Forms.Git
+{
+    public class VsoGitFileSystemProvider : IFileSystem
+    {
+        readonly IFileSystem innerFileSystem;
+        private readonly GitHttpClientBase gitClient;
+        private readonly string repositoryId;
+        private readonly string repoRoot;
+
+        public VsoGitFileSystemProvider(string virtualRoot, string gitUrl, string username, string password, string repositoryId, string repoRoot)
+            : this(
+                  new PhysicalFileSystem(virtualRoot), 
+                  new GitHttpClient(new Uri(gitUrl), new VssCredentials(new VssBasicCredential(username, password))), repositoryId, repoRoot)
+        {
+        }
+
+        public VsoGitFileSystemProvider(IFileSystem innerFileSystem, GitHttpClientBase gitClient, string repositoryId, string repoRoot)
+        {
+            this.innerFileSystem = innerFileSystem;
+            this.gitClient = gitClient;
+            this.repositoryId = repositoryId;
+            this.repoRoot = repoRoot;
+        }
+
+        public IEnumerable<string> GetDirectories(string path)
+        {
+            return innerFileSystem.GetDirectories(path);
+        }
+
+        public void DeleteDirectory(string path)
+        {
+            innerFileSystem.DeleteDirectory(path);
+        }
+
+        public void DeleteDirectory(string path, bool recursive)
+        {
+            innerFileSystem.DeleteDirectory(path, recursive);
+        }
+
+        public bool DirectoryExists(string path)
+        {
+            return innerFileSystem.DirectoryExists(path);
+        }
+
+        public void AddFile(string path, Stream stream)
+        {
+            AddFile(path, stream, true);
+        }
+
+        public void AddFile(string path, Stream stream, bool overrideIfExists)
+        {
+            innerFileSystem.AddFile(path, stream, overrideIfExists);
+
+            if (stream.CanSeek)
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                path = path.TrimStart('~');
+
+                var lastCommitId = ExecuteSync(GetLastCommitId);
+                var fileExists = ExecuteSync(() => GitFileExists(path));
+
+                VersionControlChangeType changeType;
+                string message;
+
+                if (fileExists)
+                {
+                    changeType = VersionControlChangeType.Edit;
+                    message = "Changed from backoffice";
+                }
+                else
+                {
+                    changeType = VersionControlChangeType.Add;
+                    message = "Added from backoffice";
+                }
+
+                ExecuteSync(() => PushChange(path, stream, lastCommitId, message, changeType));
+            }
+            else
+            {
+                // TODO: Log can't seek?
+            }
+        }
+
+        private async Task<bool> GitFileExists(string path)
+        {
+            try
+            {
+                await gitClient.GetItemAsync(repositoryId, repoRoot + path);
+            }
+            catch (VssServiceException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void ExecuteSync(Func<Task> asyncCall)
+        {
+            asyncCall().SyncResult();
+        }
+
+        private TResult ExecuteSync<TResult>(Func<Task<TResult>> asyncCall)
+        {
+            return asyncCall().SyncResult();
+        }
+
+        private async Task PushChange(string path, Stream stream, string lastCommitId, string message, VersionControlChangeType changeType)
+        {
+            var fileContents = new StreamReader(stream).ReadToEnd();
+            var result = await gitClient.CreatePushAsync(new GitPush
+            {
+                RefUpdates = new[]
+                {
+                    new GitRefUpdate
+                    {
+                        Name = "refs/heads/master",
+                        OldObjectId = lastCommitId
+                    }
+                },
+                Commits = new[]
+                {
+                    new GitCommit
+                    {
+                        Comment = message,
+                        Changes = new[]
+                        {
+                            new GitChange
+                            {
+                                ChangeType = changeType,
+                                Item = new GitItem
+                                {
+                                    Path = repoRoot + path
+                                },
+                                NewContent = new ItemContent
+                                {
+                                    ContentType = ItemContentType.RawText,
+                                    Content = fileContents
+                                }
+                            }
+                        }
+                    }
+                }
+            }, repositoryId);
+        }
+
+        private async Task<string> GetLastCommitId()
+        {
+            var result = await gitClient.GetCommitsAsync(repositoryId, new GitQueryCommitsCriteria
+            {
+                ItemVersion = new GitVersionDescriptor
+                {
+                    Version = "master",
+                    VersionType = GitVersionType.Branch
+                }
+            }, 0, 1);
+            return result[0].CommitId;
+        }
+
+        public IEnumerable<string> GetFiles(string path)
+        {
+            return innerFileSystem.GetFiles(path);
+        }
+
+        public IEnumerable<string> GetFiles(string path, string filter)
+        {
+            return innerFileSystem.GetFiles(path, filter);
+        }
+
+        public Stream OpenFile(string path)
+        {
+            return innerFileSystem.OpenFile(path);
+        }
+
+        public void DeleteFile(string path)
+        {
+            innerFileSystem.DeleteFile(path);
+        }
+
+        public bool FileExists(string path)
+        {
+            return innerFileSystem.FileExists(path);
+        }
+
+        public string GetRelativePath(string fullPathOrUrl)
+        {
+            return innerFileSystem.GetRelativePath(fullPathOrUrl);
+        }
+
+        public string GetFullPath(string path)
+        {
+            return innerFileSystem.GetFullPath(path);
+        }
+
+        public string GetUrl(string path)
+        {
+            return innerFileSystem.GetUrl(path);
+        }
+
+        public DateTimeOffset GetLastModified(string path)
+        {
+            return innerFileSystem.GetLastModified(path);
+        }
+
+        public DateTimeOffset GetCreated(string path)
+        {
+            return innerFileSystem.GetCreated(path);
+        }
+    }
+}
